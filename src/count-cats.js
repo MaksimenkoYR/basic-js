d13eebcc13fc5fa93228306e87e627cf00cfce96
Maksimenko Yaroslav <maksimenko.yr@gmail.com>
2021-10-05 01:23:47 +0200

    commit

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,9 +1,9 @@
-import { NotImplementedError } from '../extensions/index.js';
+import {NotImplementedError} from '../extensions/index.js'
 
 /**
  * Given matrix where you have to find cats by ears "^^"
  *
- * @param {Array<Array>} matrix 
+ * @param {Array<Array>} matrix
  * @return {Number} count of cats found
  *
  * @example
@@ -15,6 +15,15 @@ import { NotImplementedError } from '../extensions/index.js';
  *
  */
 export default function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+    function countCats(backyard) {
+        let cats = 0
+        backyard.forEach(element => {
+            element.forEach(element => {
+                if (element === '^^') {
+                    cats++
+                }
+            })
+        })
+        return cats
+    }
 }
--- src/what-season.js
+++ src/what-season.js
@@ -1,17 +1,50 @@
-import { NotImplementedError } from '../extensions/index.js';
+import {NotImplementedError} from '../extensions/index.js'
 
 /**
  * Extract season from given date and expose the enemy scout!
- * 
+ *
  * @param {Date | FakeDate} date real or fake date
  * @returns {String} time of the year
- * 
+ *
  * @example
- * 
+ *
  * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
+ *
  */
 export default function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+    function getSeasone(date) {
+        if (!date) {
+            return 'Unable to determine the time of year!'
+        }
+
+        if (!date instanceof Date || isNaN(date)) {
+            return 'Invalid date!'
+        }
+
+        let month = date.getMonth() + 1
+        switch (month.toString()) {
+            case '12':
+            case '1':
+            case '2':
+                season = 'winter'
+                break
+            case '3':
+            case '4':
+            case '5':
+                season = 'spring'
+                break
+            case '6':
+            case '7':
+            case '8':
+                season = 'summer'
+                break
+            case '9':
+            case '10':
+            case '11':
+                season = 'fall'
+                break
+        }
+
+        return season
+    }
 }
